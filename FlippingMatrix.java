adc937d0192d1096e2151049adefd759ff34b5f2
suraj-mali-1423 <surajsmali@coep.sveri.ac.in>
2025-03-18 21:43:24 +0530

    861. Score After Flipping Matrix You are given an m x n binary matrix grid.
    A move consists of choosing any row or column and toggling each value in that row or column (i.e., changing all 0's to 1's, and all 1's to 0's).
    
    Every row of the matrix is interpreted as a binary number, and the score of the matrix is the sum of these numbers.
    
    Return the highest possible score after making any number of moves (including zero moves).
    
    Input: grid = [[0,0,1,1],[1,0,1,0],[1,1,0,0]]
    Output: 39
    Explanation: 0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
    Example 2:
    
    Input: grid = [[0]]
    Output: 1

---
+++ FlippingMatrix.java
@@ -0,0 +1,57 @@
+public class FlippingMatrix {
+    public static void main(String[] args) {
+        int [][] arr = { {0,0,1,1}, {1,0,1,0},{1,1,0,0}};
+        int n = arr.length;
+        int m = arr[0].length;
+
+
+        for ( int i = 0 ; i < n; i++){
+           if (arr[i][0] == 0) {
+               for (int j = 0 ; j  < m ;j++){
+                  if(arr[i][j] == 0){
+                      arr[i][j] = 1;
+                  }else{
+                      arr[i][j] = 0;
+                  }
+               }
+           }
+        }
+
+        // Flip those column no. of Zero more than 1
+            for (int j = 1; j < m; j++) {
+                int noofZero = 0;
+                int noOfOne = 0 ;
+                for (int i = 0; i < n; i++) {
+                    if( arr[i][j] == 0 ) noofZero++;
+                    else noOfOne++;
+                }
+                if ( noofZero > noOfOne) {
+                    for (int i = 0; i < n; i++) {
+                        if(arr[i][j] == 0){
+                            arr[i][j] = 1;
+                        }else{
+                            arr[i][j] = 0;
+                        }
+                    }
+                }
+            }
+
+            // Addintion Of the Number
+         int Score = 0 ;
+            int X = 1;
+        for (int j = m-1; j >= 0 ; j--) {
+            for (int i = 0 ; i < n; i++){
+                Score += (arr[i][j] * X);
+            }
+            X *= 2;
+
+        }
+        System.out.println( Score);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0 ; j  < arr[0].length; j++){
+                System.out.print(arr[i][j] +"  ");
+            }
+            System.out.println();
+        }
+    }
+}
